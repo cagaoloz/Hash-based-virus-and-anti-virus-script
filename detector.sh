5682588d1c3eb0ba6107175de4477a538e5f27e4
halukçağataykir <cagatay_52@windowslive.com>
2022-06-07 02:07:10 +0300

    Update detector.sh

--- detector.sh
+++ detector.sh
@@ -23,10 +23,10 @@ done
 # Değişkenin boş olup olmadığını kontrol edin 
 if [[ "$result" == "" ]] 
 then 
-   # Ardından, virüsün algılanmadığını log kaydedin 
-   echo "Virus not found, logs written to $log_file" 
-   echo "$(date +"%b %d %T") $(hostname) $(whoami) $hash \"No virus found\"" >> $log_file 
-   else 
-   echo "Virus detected, logs written to $log_file" 
-   echo "$(date +"%b %d %T") $(hostname) $(whoami) $hash $result" >> $log_file 
+  # Ardından, virüsün algılanmadığını log kaydedin 
+  echo "Virus not found, logs written to $log_file" 
+  echo "$(date +"%b %d %T") $(hostname) $(whoami) $hash \"No virus found\"" >> $log_file 
+else 
+  echo "Virus detected, logs written to $log_file" 
+  echo "$(date +"%b %d %T") $(hostname) $(whoami) $hash $result" >> $log_file 
 fi 
